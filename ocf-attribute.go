5c04d3d58f5fc83414b3cbf3e62499687b463671
Jozef Kralik <jozef.kralik@kistler.com>
2018-07-25 16:28:20 +0200

    design of interfaces

---
+++ ocf-attribute.go
@@ -0,0 +1,62 @@
+package main
+
+type OCFAttributeI interface {
+	IdI
+	GetValue() (interface{}, error)
+	SetValue(s interface{}) (bool, error)
+}
+
+type OCFAttribute struct {
+	Id    IdI
+	Value OCFValueI
+	Limit OCFLimitI
+}
+
+func (a *OCFAttribute) GetValue() (interface{}, error) {
+	switch v := a.Value.(type) {
+	case OCFBoolValueGetI:
+		return v.Get()
+	case OCFEnumValueGetI:
+		return v.Get()
+	case OCFIntValueGetI:
+		return v.Get()
+	case OCFDoubleValueGetI:
+		return v.Get()
+	case OCFStringValueGetI:
+		return v.Get()
+	case OCFBinaryValueGetI:
+		return v.Get()
+	case OCFArrayValueGetI:
+		return v.Get()
+	case OCFMapValueGetI:
+		return v.Get()
+	}
+
+	return nil, ErrAccessDenied
+}
+
+func (a *OCFAttribute) SetValue(s interface{}) (bool, error) {
+	if err := a.Limit.ValidateValue(s); err != nil {
+		return false, err
+	}
+	switch v := a.Value.(type) {
+	case OCFBoolValueSetI:
+		return v.Set(s.(bool))
+	case OCFEnumValueSetI:
+		return v.Set(s.(string))
+	case OCFIntValueSetI:
+		return v.Set(s.(int))
+	case OCFDoubleValueSetI:
+		return v.Set(s.(float64))
+	case OCFStringValueSetI:
+		return v.Set(s.(string))
+	case OCFBinaryValueSetI:
+		return v.Set(s.([]byte))
+	case OCFArrayValueSetI:
+		return v.Set(s.([]interface{}))
+	case OCFMapValueSetI:
+		return v.Set(s.(map[string]interface{}))
+	}
+
+	return false, ErrAccessDenied
+}
---
+++ ocf-limit.go
@@ -0,0 +1,146 @@
+package main
+
+type OCFLimitI interface {
+	ValidateValue(interface{}) error
+}
+
+type OCFBoolLimit struct {
+}
+
+func (a *OCFBoolLimit) ValidateValue(val interface{}) error {
+	switch val.(type) {
+	case *bool:
+		return nil
+	default:
+		return ErrInvalidType
+	}
+}
+
+type OCFEnumLimit struct {
+	ValidValues []string
+}
+
+func (a *OCFEnumLimit) ValidateValue(val interface{}) error {
+	switch val.(type) {
+	case *string:
+		for _, v := range a.ValidValues {
+			if v == *val.(*string) {
+				return nil
+			}
+		}
+		return ErrInvalidEnumValue
+	default:
+		return ErrInvalidType
+	}
+}
+
+type OCFIntLimit struct {
+	Limit func(val int) error
+}
+
+func (a *OCFIntLimit) ValidateValue(val interface{}) error {
+	switch val.(type) {
+	case *int:
+		if a.Limit != nil {
+			return a.Limit(*val.(*int))
+		}
+		return nil
+	default:
+		return ErrInvalidType
+	}
+}
+
+type OCFStringLimit struct {
+	Limit func(val *string) error
+}
+
+func (a *OCFStringLimit) ValidateValue(val interface{}) error {
+	switch val.(type) {
+	case *string:
+		if a.Limit != nil {
+			return a.Limit(val.(*string))
+		}
+		return nil
+	default:
+		return ErrInvalidType
+	}
+}
+
+type OCFDoubleLimit struct {
+	Limit func(val float64) error
+}
+
+func (a *OCFDoubleLimit) ValidateValue(val interface{}) error {
+	switch val.(type) {
+	case *float32:
+		if a.Limit != nil {
+			return a.Limit(*val.(*float64))
+		}
+		return nil
+	case *float64:
+		if a.Limit != nil {
+			return a.Limit(*val.(*float64))
+		}
+		return nil
+	default:
+		return ErrInvalidType
+	}
+}
+
+type OCFByteLimit struct {
+	Limit func(val []byte) error
+}
+
+func (a *OCFByteLimit) ValidateValue(val interface{}) error {
+	switch val.(type) {
+	case []byte:
+		if a.Limit != nil {
+			return a.Limit(val.([]byte))
+		}
+		return nil
+	default:
+		return ErrInvalidType
+	}
+}
+
+type OCFMapLimit struct {
+	MapLimit map[string]OCFLimitI
+}
+
+func (a *OCFMapLimit) ValidateValue(val interface{}) error {
+	switch val.(type) {
+	case map[string]interface{}:
+		m := val.(map[string]interface{})
+		for key, v := range m {
+			if l, ok := a.MapLimit[key]; ok {
+				if err := l.ValidateValue(v); err != nil {
+					return err
+				}
+			} else {
+				return ErrInvalidKeyOfMap
+			}
+		}
+		return nil
+	default:
+		return ErrInvalidType
+	}
+}
+
+type OCFArrayLimit struct {
+	Limit OCFLimitI
+}
+
+func (a *OCFArrayLimit) ValidateValue(val interface{}) error {
+	switch val.(type) {
+	case []interface{}:
+		m := val.([]interface{})
+		for _, v := range m {
+			if err := a.Limit.ValidateValue(v); err != nil {
+				return err
+			}
+		}
+		return nil
+	default:
+		return ErrInvalidType
+	}
+}
