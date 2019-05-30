d5b2ffb23c52dc5676a58d324a892308180ee2c0
Michal Cizmazia <micizma@gmail.com>
2019-05-30 12:46:08 +0200

    Add cloud client

---
+++ cloud/client.go
@@ -0,0 +1,99 @@
+package cloud
+
+import (
+	"context"
+
+	"github.com/go-ocf/grpc-gateway/pb"
+	"github.com/go-ocf/kit/net/grpc"
+	"github.com/go-ocf/kit/security"
+	"github.com/go-ocf/kit/strings"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// NewClient constructs a new OCF cloud client.
+func NewClient(addr string, tls *security.TLSConfig) (*Client, error) {
+	conn, err := grpc.NewClientConn(addr, tls)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "could not create gateway client: %v", err)
+	}
+	client := Client{gateway: pb.NewGrpcGatewayClient(conn)}
+	return &client, nil
+}
+
+// Client for communication with the OCF Cloud.
+type Client struct {
+	gateway pb.GrpcGatewayClient
+}
+
+func (c *Client) GetDevicesViaCallback(ctx context.Context, deviceIDs, resourceTypes []string, callback func(pb.Device)) error {
+	it := c.GetDevices(ctx, deviceIDs, resourceTypes...)
+	defer it.Close()
+	var v pb.Device
+	for it.Next(&v) {
+		callback(v)
+	}
+	return it.Err
+}
+
+func (c *Client) GetResourceLinksViaCallback(ctx context.Context, deviceIDs, resourceTypes []string, callback func(pb.ResourceLink)) error {
+	it := c.GetResourceLinks(ctx, deviceIDs, resourceTypes...)
+	defer it.Close()
+	var v pb.ResourceLink
+	for it.Next(&v) {
+		callback(v)
+	}
+	return it.Err
+}
+
+type TypeCallback struct {
+	Type     string
+	Callback func(pb.ResourceValue)
+}
+
+func MakeTypeCallback(resourceType string, callback func(pb.ResourceValue)) TypeCallback {
+	return TypeCallback{Type: resourceType, Callback: callback}
+}
+
+func (c *Client) RetrieveResourcesByType(
+	ctx context.Context,
+	deviceIDs []string,
+	typeCallbacks ...TypeCallback,
+) error {
+	tc := make(map[string]func(pb.ResourceValue), len(typeCallbacks))
+	resourceTypes := make([]string, 0, len(typeCallbacks))
+	for _, c := range typeCallbacks {
+		tc[c.Type] = c.Callback
+		resourceTypes = append(resourceTypes, c.Type)
+	}
+
+	it := c.RetrieveResources(ctx, deviceIDs, resourceTypes...)
+	defer it.Close()
+	var v pb.ResourceValue
+	for it.Next(&v) {
+		// TODO validate v.GetContent().GetContentType()
+
+		for _, rt := range resourceTypes {
+			if strings.SliceContains(v.Types, rt) {
+				tc[rt](v)
+				break
+			}
+		}
+	}
+	return it.Err
+}
+
+func (c *Client) GetDevices(ctx context.Context, deviceIDs []string, resourceTypes ...string) *grpc.Iterator {
+	r := pb.GetDevicesRequest{DeviceIdsFilter: deviceIDs, TypeFilter: resourceTypes}
+	return grpc.NewIterator(c.gateway.GetDevices(ctx, &r))
+}
+
+func (c *Client) GetResourceLinks(ctx context.Context, deviceIDs []string, resourceTypes ...string) *grpc.Iterator {
+	r := pb.GetResourceLinksRequest{DeviceIdsFilter: deviceIDs, TypeFilter: resourceTypes}
+	return grpc.NewIterator(c.gateway.GetResourceLinks(ctx, &r))
+}
+
+func (c *Client) RetrieveResources(ctx context.Context, deviceIDs []string, resourceTypes ...string) *grpc.Iterator {
+	r := pb.RetrieveResourcesValuesRequest{DeviceIdsFilter: deviceIDs, TypeFilter: resourceTypes}
+	return grpc.NewIterator(c.gateway.RetrieveResourcesValues(ctx, &r))
+}
