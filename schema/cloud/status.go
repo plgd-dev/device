e538c4edd9e17be8abd0c5c7b592ef2b4b609c78
Jozef Kralik <jojo.lwin@gmail.com>
2019-07-26 16:26:32 +0200

    update cloud status schema

--- schema/cloud/status.go
+++ schema/cloud/status.go
@@ -2,17 +2,17 @@ package cloud
 
 const StatusResourceType string = "x.cloud.device.status"
 
-var StatusInterfaces = []string{"oic.if.baseline", "oic.if.r"}
+var StatusInterfaces = []string{"oic.if.baseline"}
 var StatusResourceTypes = []string{StatusResourceType}
 
-const StatusHref = "/CoapCloudStatusResURI"
+const StatusHref = "/oic/cloud/s"
 
 // Status is resource published by OCF Cloud.
 // - signup: resource published
-// - signin: content changed - online true
-// - signout/close connection: content changed - online false
+// - signin: content changed -> online true
+// - signout/close connection: content changed -> online false
 type Status struct {
 	ResourceTypes []string `codec:"rt"`
 	Interfaces    []string `codec:"if"`
-	Online        bool     `codec:"apn"`
+	Online        bool     `codec:"online"`
 }
