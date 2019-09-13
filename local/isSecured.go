7c577312a52c0daa4d77696ff92d17f18d82d8c5
Jozef Kralik <jojo.lwin@gmail.com>
2019-09-13 13:10:36 +0200

    use links in call IsSecure

--- local/isSecured.go
+++ local/isSecured.go
@@ -1,10 +1,14 @@
 package local
 
-import "context"
+import (
+	"context"
+
+	"github.com/go-ocf/sdk/schema"
+)
 
 // IsSecured returns if device serves secure ports.
-func (d *Device) IsSecured(ctx context.Context) (bool, error) {
-	for _, link := range d.links {
+func (d *Device) IsSecured(ctx context.Context, links schema.ResourceLinks) (bool, error) {
+	for _, link := range links {
 		if _, err := link.GetTCPSecureAddr(); err == nil {
 			return true, nil
 		}
