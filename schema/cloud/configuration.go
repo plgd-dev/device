173ed5d00eb3988b499e9513f36c153bdb86882e
Jozef Kralik <jojo.lwin@gmail.com>
2020-01-03 10:39:18 +0100

    read provisioning status from cloud conf resource (#59)

--- schema/cloud/configuration.go
+++ schema/cloud/configuration.go
@@ -1,18 +1,30 @@
 package cloud
 
 // Cloud Configuration Resource
-// https://github.com/openconnectivityfoundation/core-extensions/blob/master/swagger2.0/oic.r.coapcloudconf.swagger.json
+// https://github.com/openconnectivityfoundation/cloud-services/blob/master/swagger2.0/oic.r.coapcloudconf.swagger.json
 
 const ConfigurationResourceType string = "oic.r.coapcloudconf"
 
+// ProvisioningStatus indicates the Cloud Provisioning status of the Device.
+type ProvisioningStatus string
+
+const (
+	ProvisioningStatus_UNINITIALIZED     ProvisioningStatus = "uninitialized"
+	ProvisioningStatus_READY_TO_REGISTER ProvisioningStatus = "readytoregister"
+	ProvisioningStatus_REGISTERING       ProvisioningStatus = "registering"
+	ProvisioningStatus_REGISTERED        ProvisioningStatus = "registered"
+	ProvisioningStatus_FAILED            ProvisioningStatus = "failed"
+)
+
 type Configuration struct {
-	ResourceTypes         []string `codec:"rt"`
-	Interfaces            []string `codec:"if"`
-	Name                  string   `codec:"n"`
-	AuthorizationProvider string   `codec:"apn"`
-	CloudID               string   `codec:"sid"`
-	URL                   string   `codec:"cis"`
-	LastErrorCode         int      `codec:"clec"`
+	ResourceTypes         []string           `codec:"rt"`
+	Interfaces            []string           `codec:"if"`
+	Name                  string             `codec:"n"`
+	AuthorizationProvider string             `codec:"apn"`
+	CloudID               string             `codec:"sid"`
+	URL                   string             `codec:"cis"`
+	LastErrorCode         int                `codec:"clec"`
+	ProvisioningStatus    ProvisioningStatus `codec:"cps"`
 }
 
 type ConfigurationUpdateRequest struct {
