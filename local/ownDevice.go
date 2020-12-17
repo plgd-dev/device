f4016c9e2d1bdc4cacf45bebc143ee7eae530c57
Jozef Kralik <jozef.kralik@kistler.com>
2020-12-17 09:54:49 +0100

    set ACL for cloud during onboard

--- local/ownDevice.go
+++ local/ownDevice.go
@@ -2,71 +2,9 @@ package local
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/plgd-dev/sdk/local/core"
-	"github.com/plgd-dev/sdk/schema/cloud"
-
-	"github.com/plgd-dev/sdk/schema"
-	"github.com/plgd-dev/sdk/schema/acl"
 )
-
-func setACLForCloudResources(ctx context.Context, p *core.ProvisioningClient, links schema.ResourceLinks) error {
-	link, err := core.GetResourceLink(links, "/oic/sec/acl2")
-	if err != nil {
-		return err
-	}
-	ownerID, err := p.GetSdkOwnerID()
-	if err != nil {
-		return err
-	}
-
-	aclResources := make([]acl.Resource, 0, 1)
-	for _, res := range links.GetResourceLinks(cloud.ConfigurationResourceType) {
-		aclResources = append(aclResources, acl.Resource{
-			Href:       res.Href,
-			Interfaces: []string{"*"},
-		})
-	}
-	if len(aclResources) == 0 {
-		return fmt.Errorf("cannot find %v resource", cloud.ConfigurationResourceType)
-	}
-
-	obACL := acl.UpdateRequest{
-		AccessControlList: []acl.AccessControl{
-			acl.AccessControl{
-				Permission: acl.AllPermissions,
-				Subject: acl.Subject{
-					Subject_Device: &acl.Subject_Device{
-						DeviceID: ownerID,
-					},
-				},
-				Resources: aclResources,
-			},
-		},
-	}
-
-	return p.UpdateResource(ctx, link, obACL, nil)
-}
-
-func configureDeviceInProvsion(ctx context.Context, d *RefDevice, links schema.ResourceLinks) (rerr error) {
-	p, err := d.Provision(ctx, links)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		err := p.Close(ctx)
-		if err != nil && rerr == nil {
-			rerr = err
-		}
-	}()
-
-	err = setACLForCloudResources(ctx, p, links)
-	if err != nil {
-		return fmt.Errorf("cannot set acl for cloud resources: %w", err)
-	}
-	return nil
-}
 
 func (c *Client) OwnDevice(ctx context.Context, deviceID string, opts ...OwnOption) (string, error) {
 	cfg := ownOptions{
@@ -121,12 +59,6 @@ func (c *Client) ownDeviceWithSigners(ctx context.Context, deviceID string, otmC
 
 	err = d.Own(ctx, links, otmClient, opts...)
 	if err != nil {
-		return "", err
-	}
-
-	err = configureDeviceInProvsion(ctx, d, links)
-	if err != nil {
-		d.Disown(ctx, links)
 		return "", err
 	}
 
