bac2301b8229085650ae070468e8ee9e6ba8ec05
Daniel Adam <daniel.adam1922@protonmail.com>
2021-10-15 23:34:09 +0200

    Add oic.r.account resource

---
+++ schema/account/account.go
@@ -0,0 +1,8 @@
+// Account Token
+// https://github.com/openconnectivityfoundation/security/blob/master/swagger2.0/oic.sec.account.swagger.json
+package account
+
+const (
+	ResourceType = "oic.r.account"
+	ResourceURI  = "/oic/sec/account"
+)
