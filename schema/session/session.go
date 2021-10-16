7dd207ce67a1785554bf41b3a6036d7f1c6fe186
Daniel Adam <daniel.adam1922@protonmail.com>
2021-10-16 12:47:13 +0200

    Add oic.r.session resource

---
+++ schema/session/session.go
@@ -0,0 +1,9 @@
+// Session
+// Resource that manages the persistent session between a Device and OCF Cloud.
+// https://github.com/openconnectivityfoundation/security/blob/master/swagger2.0/oic.sec.session.swagger.json
+package session
+
+const (
+	ResourceType = "oic.r.session"
+	ResourceURI  = "/oic/sec/session"
+)
