45452acda4160e2684e34a557071cc7f1dd36cc0
Jozef Kralik <jozef.kralik@kistler.com>
2022-04-29 09:51:03 +0200

    fix - don't use device after release

--- client/getDevice.go
+++ client/getDevice.go
@@ -51,10 +51,11 @@ func (c *Client) GetRefDeviceByIP(
 	}
 	links, err := getLinksRefDevice(ctx, refDev, c.disableUDPEndpoints)
 	if err != nil {
+		deviceID := refDev.DeviceID()
 		refDev.Device().Close(ctx)
-		c.deviceCache.RemoveDevice(ctx, refDev.DeviceID(), refDev)
+		c.deviceCache.RemoveDevice(ctx, deviceID, refDev)
 		refDev.Release(ctx)
-		return nil, nil, fmt.Errorf("cannot get links for device %v: %w", refDev.DeviceID(), err)
+		return nil, nil, fmt.Errorf("cannot get links for device %v: %w", deviceID, err)
 	}
 	return refDev, patchResourceLinksEndpoints(links, c.disableUDPEndpoints), nil
 }
