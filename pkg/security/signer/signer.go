0ce689377814e710177cd98c6d489a20a7f1bef9
Jozef Kralik <jojo.lwin@gmail.com>
2023-01-09 10:09:25 +0000

    add tests

--- pkg/security/signer/signer.go
+++ pkg/security/signer/signer.go
@@ -28,11 +28,24 @@ func NewOCFIdentityCertificate(caCert []*x509.Certificate, caKey crypto.PrivateK
 
 func (s *OCFIdentityCertificate) Sign(ctx context.Context, csr []byte) (signedCsr []byte, err error) {
 	now := time.Now()
-	if now.Before(s.validNotBefore) {
-		return nil, fmt.Errorf("not valid yet: current time %v is out of time range %v-%v", now, s.validNotBefore, s.validNotAfter)
+	notBefore := s.validNotBefore
+	notAfter := s.validNotAfter
+	for _, c := range s.caCert {
+		if notBefore.Before(c.NotBefore) {
+			notBefore = c.NotBefore
+		}
+		if notAfter.After(c.NotAfter) {
+			notAfter = c.NotAfter
+		}
 	}
-	if now.After(s.validNotAfter) {
-		return nil, fmt.Errorf("expired: current time %v is out of time range %v-%v", now, s.validNotBefore, s.validNotAfter)
+	if notBefore.After(notAfter) {
+		return nil, fmt.Errorf("invalid time range: not before %v limit is greater than not after limit %v", notBefore.Format(time.RFC3339), notAfter.Format(time.RFC3339))
+	}
+	if now.Before(notBefore) {
+		return nil, fmt.Errorf("not valid yet: current time %v is out of time range: %v <-> %v", now, notBefore.Format(time.RFC3339), notAfter.Format(time.RFC3339))
+	}
+	if now.After(notAfter) {
+		return nil, fmt.Errorf("expired: current time %v is out of time range: %v <-> %v", now, notBefore.Format(time.RFC3339), notAfter.Format(time.RFC3339))
 	}
 
 	csrBlock, _ := pem.Decode(csr)
@@ -51,8 +64,6 @@ func (s *OCFIdentityCertificate) Sign(ctx context.Context, csr []byte) (signedCs
 		return
 	}
 
-	notBefore := s.validNotBefore
-	notAfter := s.validNotAfter
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
