15497b9982fe825e614b9227907a96e64a68cac2
Daniel Adam <daniel.adam1922@protonmail.com>
2023-11-06 15:42:55 +0100

    Add ResourceLink tests

--- schema/link_test.go
+++ schema/link_test.go
@@ -27,6 +27,119 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestEndpointGetAddress(t *testing.T) {
+	type args struct {
+		uri string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    kitNet.Addr
+		wantErr bool
+	}{
+		{
+			name: "Invalid",
+			args: args{
+				uri: "invalid-uri",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Valid",
+			args: args{
+				uri: "tcp://example.com:12345",
+			},
+			want: kitNet.MakeAddr("tcp", "example.com", 12345),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ep := schema.Endpoint{
+				URI: tt.args.uri,
+			}
+			addr, err := ep.GetAddr()
+			if tt.wantErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tt.want, addr)
+		})
+	}
+}
+
+func TestEndpointsGetAddrInvalid(t *testing.T) {
+	endpoints := schema.Endpoints{}
+	_, err := endpoints.GetAddr(schema.TCPScheme)
+	require.Error(t, err)
+
+	endpoints = schema.Endpoints{
+		schema.Endpoint{URI: "not-valid-uri", Priority: 2},
+		schema.Endpoint{URI: "coaps+tcp://exampleTLS.com:123", Priority: 3},
+		schema.Endpoint{URI: "coaps://exampleDTLS.com:789", Priority: 1},
+	}
+	_, err = endpoints.GetAddr(schema.TCPScheme)
+	require.Error(t, err)
+}
+
+func TestEndpointsGetAddr(t *testing.T) {
+	endpoints := schema.Endpoints{
+		schema.Endpoint{URI: "coaps+tcp://exampleTLS.com:123", Priority: 3},
+		schema.Endpoint{URI: "coap+tcp://exampleTCP.com:456", Priority: 2},
+		schema.Endpoint{URI: "coaps://exampleDTLS.com:789", Priority: 1},
+	}
+
+	_, err := endpoints.GetAddr(schema.UDPScheme)
+	require.Error(t, err)
+	addr, err := endpoints.GetAddr(schema.TCPSecureScheme)
+	require.NoError(t, err)
+	require.Equal(t, kitNet.MakeAddr(string(schema.TCPSecureScheme), "exampleTLS.com", 123), addr)
+	addr, err = endpoints.GetAddr(schema.TCPScheme)
+	require.NoError(t, err)
+	require.Equal(t, kitNet.MakeAddr(string(schema.TCPScheme), "exampleTCP.com", 456), addr)
+	addr, err = endpoints.GetAddr(schema.UDPSecureScheme)
+	require.NoError(t, err)
+	require.Equal(t, kitNet.MakeAddr(string(schema.UDPSecureScheme), "exampleDTLS.com", 789), addr)
+}
+
+func TestResourceLinkGetSecureEndpoints(t *testing.T) {
+	eps := schema.Endpoints{}
+	require.Len(t, eps.FilterSecureEndpoints(), 0)
+
+	rls := schema.ResourceLink{
+		Endpoints: schema.Endpoints{
+			schema.Endpoint{URI: "coap://example.com:5683"},
+			schema.Endpoint{URI: "coap+tcp://example.com:5683"},
+			schema.Endpoint{URI: "coaps://example.com:5684"},
+			schema.Endpoint{URI: "coaps+tcp://example.com:5684"},
+		},
+	}
+
+	seps := rls.GetSecureEndpoints()
+	require.Len(t, seps, 2)
+	require.Equal(t, "coaps://example.com:5684", seps[0].URI)
+	require.Equal(t, "coaps+tcp://example.com:5684", seps[1].URI)
+}
+
+func TestResourceLinkGetUnsecureEndpoints(t *testing.T) {
+	eps := schema.Endpoints{}
+	require.Len(t, eps.FilterUnsecureEndpoints(), 0)
+
+	rls := schema.ResourceLink{
+		Endpoints: schema.Endpoints{
+			schema.Endpoint{URI: "coap://example.com:5683"},
+			schema.Endpoint{URI: "coap+tcp://example.com:5683"},
+			schema.Endpoint{URI: "coaps://example.com:5684"},
+			schema.Endpoint{URI: "coaps+tcp://example.com:5684"},
+		},
+	}
+
+	useps := rls.GetUnsecureEndpoints()
+	require.Len(t, useps, 2)
+	require.Equal(t, "coap://example.com:5683", useps[0].URI)
+	require.Equal(t, "coap+tcp://example.com:5683", useps[1].URI)
+}
+
 func TestBitMaskHas(t *testing.T) {
 	b := schema.BitMask(0)
 	require.False(t, b.Has(schema.Discoverable))
@@ -97,7 +210,8 @@ func TestResourceLinkHasType(t *testing.T) {
 
 func TestResourceLinkPatchEndpoint(t *testing.T) {
 	type args struct {
-		addr kitNet.Addr
+		addr    kitNet.Addr
+		secured bool
 	}
 	tests := []struct {
 		name string
@@ -116,12 +230,36 @@ func TestResourceLinkPatchEndpoint(t *testing.T) {
 			},
 		},
 		{
+			name: "IPv4-secure",
+			args: args{
+				addr:    kitNet.MakeAddr("coap", "127.0.0.1", 5683),
+				secured: true,
+			},
+			want: schema.Endpoints{
+				schema.Endpoint{URI: "coaps://127.0.0.1:5683"},
+				schema.Endpoint{URI: "coap+tcp://127.0.0.1:5683"},
+				schema.Endpoint{URI: "coaps+tcp://127.0.0.1:5684"},
+			},
+		},
+		{
 			name: "IPv6",
 			args: args{
 				addr: kitNet.MakeAddr("coap", "fe80::1", 5683),
 			},
 			want: schema.Endpoints{
 				schema.Endpoint{URI: "coap://[fe80::1]:5683"},
+				schema.Endpoint{URI: "coap+tcp://[fe80::1]:5683"},
+				schema.Endpoint{URI: "coaps+tcp://[fe80::1]:5684"},
+			},
+		},
+		{
+			name: "IPv6-secure",
+			args: args{
+				addr:    kitNet.MakeAddr("coap", "fe80::1", 5683),
+				secured: true,
+			},
+			want: schema.Endpoints{
+				schema.Endpoint{URI: "coaps://[fe80::1]:5683"},
 				schema.Endpoint{URI: "coap+tcp://[fe80::1]:5683"},
 				schema.Endpoint{URI: "coaps+tcp://[fe80::1]:5684"},
 			},
@@ -134,6 +272,7 @@ func TestResourceLinkPatchEndpoint(t *testing.T) {
 					UDPPort:    5683,
 					TCPPort:    5683,
 					TCPTLSPort: 5684,
+					Secured:    tt.args.secured,
 				},
 			}
 			got := r.PatchEndpoint(tt.args.addr, nil)
@@ -211,4 +350,97 @@ func TestResourceLinkPatchEndpointLinkLocal(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestResourceLinkGetTCPAddr(t *testing.T) {
+	resourceLink := schema.ResourceLink{
+		Endpoints: schema.Endpoints{
+			schema.Endpoint{URI: "coap://exampleUDP.com:78", Priority: 4},
+			schema.Endpoint{URI: "coaps+tcp://exampleTLS.com:12", Priority: 1},
+			schema.Endpoint{URI: "coap+tcp://exampleTCP1.com:34", Priority: 2},
+			schema.Endpoint{URI: "coap+tcp://exampleTCP2.com:56", Priority: 3},
+		},
+	}
+
+	addr, err := resourceLink.GetTCPAddr()
+	require.NoError(t, err)
+	require.Equal(t, kitNet.MakeAddr("coap+tcp", "exampleTCP1.com", 34), addr)
+}
+
+func TestResourceLinkGetTCPSecureAddr(t *testing.T) {
+	resourceLink := schema.ResourceLink{
+		Endpoints: schema.Endpoints{
+			schema.Endpoint{URI: "coap+tcp://exampleTCP.com:12", Priority: 1},
+			schema.Endpoint{URI: "coaps+tcp://exampleTLS1.com:34", Priority: 2},
+			schema.Endpoint{URI: "coaps+tcp://exampleTLS2.com:56", Priority: 3},
+			schema.Endpoint{URI: "coap://exampleUDP.com:78", Priority: 4},
+		},
+	}
+
+	addr, err := resourceLink.GetTCPSecureAddr()
+	require.NoError(t, err)
+	require.Equal(t, kitNet.MakeAddr("coaps+tcp", "exampleTLS1.com", 34), addr)
+}
+
+func TestGetUDPAddr(t *testing.T) {
+	resourceLink := schema.ResourceLink{
+		Endpoints: schema.Endpoints{
+			schema.Endpoint{URI: "coap://exampleUDP.com:12", Priority: 1},
+			schema.Endpoint{URI: "coaps+tcp://exampleTLS1.com:34", Priority: 2},
+			schema.Endpoint{URI: "coap+tcp://exampleTCP.com:56", Priority: 3},
+			schema.Endpoint{URI: "coaps+tcp://exampleTLS2.com:78", Priority: 4},
+		},
+	}
+
+	addr, err := resourceLink.GetUDPAddr()
+	require.NoError(t, err)
+	require.Equal(t, kitNet.MakeAddr("coap", "exampleUDP.com", 12), addr)
+}
+
+func TestGetUDPSecureAddr(t *testing.T) {
+	resourceLink := schema.ResourceLink{
+		Endpoints: schema.Endpoints{
+			schema.Endpoint{URI: "coap+tcp://exampleTCP.com:34", Priority: 2},
+			schema.Endpoint{URI: "coaps+tcp://exampleTLS.com:56", Priority: 3},
+			schema.Endpoint{URI: "coap://exampleUDP.com:78", Priority: 4},
+			schema.Endpoint{URI: "coaps://exampleDTLS.com:12", Priority: 1},
+		},
+	}
+
+	addr, err := resourceLink.GetUDPSecureAddr()
+	require.NoError(t, err)
+	require.Equal(t, kitNet.MakeAddr("coaps", "exampleDTLS.com", 12), addr)
+}
+
+func TestResourceLinkGetDeviceID(t *testing.T) {
+	rl := schema.ResourceLink{}
+	require.Equal(t, "", rl.GetDeviceID())
+
+	rl = schema.ResourceLink{
+		DeviceID: "device-id",
+	}
+	require.Equal(t, "device-id", rl.GetDeviceID())
+
+	rl = schema.ResourceLink{
+		Anchor: "ocf://device-id",
+	}
+	require.Equal(t, "device-id", rl.GetDeviceID())
+}
+
+func TestResourceLinksEndpointsSort(t *testing.T) {
+	endpoints := schema.Endpoints{}
+	sortedEndpoints := endpoints.Sort()
+	require.Equal(t, 0, len(sortedEndpoints))
+
+	endpoints = schema.Endpoints{
+		schema.Endpoint{URI: "tcp://example.com:12345", Priority: 2},
+		schema.Endpoint{URI: "udp://example.com:54321", Priority: 1},
+		schema.Endpoint{URI: "coap://example.com:5683", Priority: 3},
+	}
+
+	sortedEndpoints = endpoints.Sort()
+	require.Equal(t, 3, len(sortedEndpoints))
+	require.Equal(t, "udp://example.com:54321", sortedEndpoints[0].URI)
+	require.Equal(t, "tcp://example.com:12345", sortedEndpoints[1].URI)
+	require.Equal(t, "coap://example.com:5683", sortedEndpoints[2].URI)
 }
